36606c693bc7ac8f2ae643e76328f5ce7ce64da5
sabrinaValansi <sabrivalan@hotmail.com>
2022-10-28 20:18:45 -0300

    primer commit

---
+++ app/src/main/java/ar/edu/ort/proyectosabrina/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package ar.edu.ort.proyectosabrina.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ar.edu.ort.proyectosabrina.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
